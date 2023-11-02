2df18adc37f237304a9163c92a574bad2e78e145
Camila Caldas <camisccb@gmail.com>
2023-11-02 19:01:55 -0300

    add ambiente virtual project

---
+++ Ambiente Virtual/Python-scripts/scriptexample2.py
@@ -0,0 +1,87 @@
+import sys
+import os
+
+
+# Piece of code to include site package of virtual environment in sys path first position
+
+SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+site_package_venv = os.path.join(SCRIPT_DIRECTORY, ".venv", "Lib", "site-packages")
+sys.path.insert(0, site_package_venv)
+
+
+import logging
+import datetime
+import pandas as pd
+
+
+def include_info_execution():
+    """Includes execution information in a log file"""
+
+    # Path to libs used
+    logging.info(f"Import path pandas: {pd.__file__}")
+
+    # Path to Python execution
+    python_execution = sys.executable
+    logging.info(f"Python execution file: {python_execution}")
+
+    # List of directories where Python searches dependencies
+    directories_list = sys.path
+    diretory_str = "\n".join(directories_list)
+    logging.info(
+        f"List of directories where Python searches dependencies: \n{diretory_str}"
+    )
+
+
+def set_log_file():
+    """Sets path for log file and its"""
+
+    # Gets current date and hour
+    current_datetime = datetime.datetime.now()
+
+    # Format date and hour
+    date_time_str = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Define log file path
+    project_folder = os.path.dirname(SCRIPT_DIRECTORY)
+    log_file_path = os.path.join(
+        project_folder, f"EvidencePython_{date_time_str}.log"
+    )
+
+    # handle no writing in log file
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    # Sets log format
+    logging.basicConfig(
+        filename=log_file_path,
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+    )
+
+
+def main(num1_str: str,num2_str: str):
+    """
+    Receive 2 numbers and write in a sheet
+    """
+
+    # Set log file
+    set_log_file()
+    logging.info("Log file created")
+
+    # Include execution information
+    include_info_execution()
+
+    # Transform string to integer
+    num1_int = int(num1_str)
+    num2_int = int(num2_str)
+
+    # Create dataframe
+    data = {'Number 1': [num1_int], 'Number 2': [num2_int]}
+    df = pd.DataFrame(data)
+
+    # Define excel file path
+    excel_file_name = 'numbers.xlsx'
+
+    # Write dataframe in excel
+    df.to_excel(excel_file_name, index=False)
+    logging.info("Received data was inlcuded in excel")
